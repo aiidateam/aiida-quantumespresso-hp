e3b9484d0d0adcd40070290bac05ce214658d2a3
Sebastiaan Huber <mail@sphuber.net>
2017-06-19 20:07:00 +0200

    Automatically register with reentry upon setup

--- setup.py
+++ setup.py
@@ -7,5 +7,9 @@ if __name__ == '__main__':
     setup(
         include_package_data=True,
         packages=find_packages(),
+        setup_requires=[
+            'reentry'
+        ],
+        reentry_register=True,
         **kwargs
     )
