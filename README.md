99527e8bf9c5ab73bf251260a1c516745bd078ab
Sebastiaan Huber <mail@sphuber.net>
2019-10-05 09:36:20 +0200

    Add pre-commit hooks `pylint` and `yapf`

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 AiiDA plugin for the Hubbard module of Quantum ESPRESSO
 
 # Requirements
-This package depends directly on `aiida-core >= v0.9.0` and the `aiida-quantumespresso` package.
+This package depends directly on `aiida-core>=v1.0.0` and the `aiida-quantumespresso>=3.0.0` package.
--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
+# -*- coding: utf-8 -*-
+"""Setup script for aiida-quantumespresso-hp package."""
+from __future__ import absolute_import
 import json
+
+# pylint: disable=wrong-import-order,unused-import
+from utils import fastentrypoints
 from setuptools import setup, find_packages
 
 if __name__ == '__main__':
-    with open('setup.json', 'r') as handle:
-        kwargs = json.load(handle)
-    setup(
-        include_package_data=True,
-        packages=find_packages(),
-        setup_requires=[
-            'reentry'
-        ],
-        reentry_register=True,
-        **kwargs
-    )
+    with open('setup.json', 'r') as info:
+        SETUP_JSON = json.load(info)
+    setup(include_package_data=True, reentry_register=True, packages=find_packages(), **SETUP_JSON)
