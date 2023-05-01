79c9ff74f94eed377d3c599b7100b8c95018d0aa
bastonero <bastonero.lorenzo@gmail.com>
2023-05-01 14:55:21 +0000

    `HpBaseWorkChain`: update, adapt and improve
    The following changes are applied:
    * Introduce the `clean_workdir` input
    * Introduce automatic setting of `max_seconds`
    * Add the `get_builder_from_protocol` method

--- src/aiida_quantumespresso_hp/workflows/hp/base.py
+++ src/aiida_quantumespresso_hp/workflows/hp/base.py
@@ -2,13 +2,15 @@
 """Workchain to run a Quantum ESPRESSO hp.x calculation with automated error handling and restarts."""
 from aiida import orm
 from aiida.common import AttributeDict
+from aiida.common.lang import type_check
 from aiida.engine import BaseRestartWorkChain, ProcessHandlerReport, process_handler, while_
 from aiida.plugins import CalculationFactory
+from aiida_quantumespresso.workflows.protocols.utils import ProtocolMixin
 
 HpCalculation = CalculationFactory('quantumespresso.hp')
 
 
-class HpBaseWorkChain(BaseRestartWorkChain):
+class HpBaseWorkChain(BaseRestartWorkChain, ProtocolMixin):
     """Workchain to run a Quantum ESPRESSO hp.x calculation with automated error handling and restarts."""
 
     _process_class = HpCalculation
@@ -16,6 +18,7 @@ class HpBaseWorkChain(BaseRestartWorkChain):
     defaults = AttributeDict({
         'delta_factor_alpha_mix': 0.5,
         'delta_factor_niter_max': 2,
+        'delta_factor_max_seconds': 0.95,
     })
 
     @classmethod
@@ -25,10 +28,14 @@ class HpBaseWorkChain(BaseRestartWorkChain):
         super().define(spec)
         spec.expose_inputs(HpCalculation, namespace='hp')
         spec.input('only_initialization', valid_type=orm.Bool, default=lambda: orm.Bool(False))
+        spec.input('clean_workdir', valid_type=orm.Bool, default=lambda: orm.Bool(False),
+            help='If `True`, work directories of all called calculation will be cleaned at the end of execution.')
+
         spec.outline(
             cls.setup,
             cls.validate_parameters,
             while_(cls.should_run_process)(
+                cls.prepare_process,
                 cls.run_process,
                 cls.inspect_process,
             ),
@@ -37,6 +44,85 @@ class HpBaseWorkChain(BaseRestartWorkChain):
         spec.expose_outputs(HpCalculation)
         spec.exit_code(300, 'ERROR_UNRECOVERABLE_FAILURE',
             message='The calculation failed with an unrecoverable error.')
+
+    @classmethod
+    def get_protocol_filepath(cls):
+        """Return ``pathlib.Path`` to the ``.yaml`` file that defines the protocols."""
+        from importlib_resources import files
+
+        from ..protocols import hp as hp_protocols
+        return files(hp_protocols) / 'base.yaml'
+
+    @classmethod
+    def get_builder_from_protocol(
+        cls,
+        code,
+        protocol=None,
+        parent_scf_folder=None,
+        parent_hp_folders: dict = None,
+        overrides=None,
+        options=None,
+        **_
+    ):
+        """Return a builder prepopulated with inputs selected according to the chosen protocol.
+
+        :param code: the ``Code`` instance configured for the ``quantumespresso.hp`` plugin.
+        :param protocol: protocol to use, if not specified, the default will be used.
+        :param parent_scf_folder: the parent ``RemoteData`` of the respective SCF calcualtion.
+        :param parent_hp_folders: the parent ``FolderData`` of the respective single atoms HP calcualtions.
+        :param overrides: optional dictionary of inputs to override the defaults of the protocol.
+        :param options: A dictionary of options that will be recursively set for the ``metadata.options`` input of all
+            the ``CalcJobs`` that are nested in this work chain.
+        :return: a process builder instance with all inputs defined ready for launch.
+        """
+        from aiida_quantumespresso.workflows.protocols.utils import recursive_merge
+
+        if isinstance(code, str):
+            code = orm.load_code(code)
+
+        type_check(code, orm.AbstractCode)
+
+        inputs = cls.get_protocol_inputs(protocol, overrides)
+
+        # Update the parameters based on the protocol inputs
+        parameters = inputs['hp']['parameters']
+        metadata = inputs['hp']['metadata']
+
+        qpoints_mesh = inputs['hp'].pop('qpoints')
+        qpoints = orm.KpointsData()
+        qpoints.set_kpoints_mesh(qpoints_mesh)
+
+        # If overrides are provided, they are considered absolute
+        if overrides:
+            parameter_overrides = overrides.get('hp', {}).get('parameters', {})
+            parameters = recursive_merge(parameters, parameter_overrides)
+
+        if options:
+            metadata['options'] = recursive_merge(inputs['hp']['metadata']['options'], options)
+
+        hubbard_structure = inputs['hp'].pop('hubbard_structure', None)
+        parent_scf = parent_scf_folder if not 'parent_scf' in inputs['hp'] else inputs['hp']['parent_scf']
+        parent_hp = parent_hp_folders if not 'parent_scf' in inputs['hp'] else inputs['hp']['parent_scf']
+
+        # pylint: disable=no-member
+        builder = cls.get_builder()
+        builder.hp['code'] = code
+        builder.hp['qpoints'] = qpoints
+        builder.hp['parameters'] = orm.Dict(parameters)
+        builder.hp['metadata'] = metadata
+        if 'settings' in inputs['hp']:
+            builder.hp['settings'] = orm.Dict(inputs['hp']['settings'])
+        if hubbard_structure:
+            builder.hp['hubbard_structure'] = hubbard_structure
+        if parent_scf:
+            builder.hp['parent_scf'] = parent_scf
+        if parent_hp:
+            builder.hp['parent_hp'] = parent_hp
+        builder.only_initialization = orm.Bool(inputs['only_initialization'])
+        builder.clean_workdir = orm.Bool(inputs['clean_workdir'])
+        # pylint: enable=no-member
+
+        return builder
 
     def setup(self):
         """Call the `setup` of the `BaseRestartWorkChain` and then create the inputs dictionary in `self.ctx.inputs`.
@@ -55,6 +141,22 @@ class HpBaseWorkChain(BaseRestartWorkChain):
 
         if self.inputs.only_initialization.value:
             self.ctx.inputs.parameters['INPUTHP']['determine_num_pert_only'] = True
+
+    def set_max_seconds(self, max_wallclock_seconds):
+        """Set the `max_seconds` to a fraction of `max_wallclock_seconds` option to prevent out-of-walltime problems.
+
+        :param max_wallclock_seconds: the maximum wallclock time that will be set in the scheduler settings.
+        """
+        max_seconds_factor = self.defaults.delta_factor_max_seconds
+        max_seconds = max_wallclock_seconds * max_seconds_factor
+        self.ctx.inputs.parameters['INPUTHP']['max_seconds'] = max_seconds
+
+    def prepare_process(self):
+        """Prepare the inputs for the next calculation."""
+        max_wallclock_seconds = self.ctx.inputs.metadata.options.get('max_wallclock_seconds', None)
+
+        if max_wallclock_seconds is not None and 'max_seconds' not in self.ctx.inputs.parameters['INPUTHP']:
+            self.set_max_seconds(max_wallclock_seconds)
 
     def report_error_handled(self, node, action):
         """Report an action taken for a calculation that has failed.
@@ -111,3 +213,24 @@ class HpBaseWorkChain(BaseRestartWorkChain):
             self.report('convergence not reached, restarting')
 
         return ProcessHandlerReport(True)
+
+    def on_terminated(self):
+        """Clean the working directories of all child calculations if `clean_workdir=True` in the inputs."""
+        super().on_terminated()
+
+        if self.inputs.clean_workdir.value is False:
+            self.report('remote folders will not be cleaned')
+            return
+
+        cleaned_calcs = []
+
+        for called_descendant in self.node.called_descendants:
+            if isinstance(called_descendant, orm.CalcJobNode):
+                try:
+                    called_descendant.outputs.remote_folder._clean()  # pylint: disable=protected-access
+                    cleaned_calcs.append(called_descendant.pk)
+                except (IOError, OSError, KeyError):
+                    pass
+
+        if cleaned_calcs:
+            self.report(f"cleaned remote folders of calculations: {' '.join(map(str, cleaned_calcs))}")
---
+++ tests/workflows/protocols/hp/test_base.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""Tests for the ``HpBaseWorkChain.get_builder_from_protocol`` method."""
+from aiida.engine import ProcessBuilder
+
+from aiida_quantumespresso_hp.workflows.hp.base import HpBaseWorkChain
+
+
+def test_get_available_protocols():
+    """Test ``HpBaseWorkChain.get_available_protocols``."""
+    protocols = HpBaseWorkChain.get_available_protocols()
+    assert sorted(protocols.keys()) == ['fast', 'moderate', 'precise']
+    assert all('description' in protocol for protocol in protocols.values())
+
+
+def test_get_default_protocol():
+    """Test ``HpBaseWorkChain.get_default_protocol``."""
+    assert HpBaseWorkChain.get_default_protocol() == 'moderate'
+
+
+def test_default(fixture_code, data_regression, serialize_builder):
+    """Test ``HpBaseWorkChain.get_builder_from_protocol`` for the default protocol."""
+    code = fixture_code('quantumespresso.hp')
+    builder = HpBaseWorkChain.get_builder_from_protocol(code)
+
+    assert isinstance(builder, ProcessBuilder)
+    data_regression.check(serialize_builder(builder))
+
+
+def test_parent_scf_folder(fixture_code, generate_calc_job_node, generate_inputs_pw, generate_hubbard_structure):
+    """Test ``HpBaseWorkChain.get_builder_from_protocol`` with ``parent_scf_folder`` keyword."""
+    code = fixture_code('quantumespresso.hp')
+    inputs_pw = generate_inputs_pw()
+    inputs_pw['structure'] = generate_hubbard_structure()
+    parent_scf_folder = generate_calc_job_node('quantumespresso.pw', inputs=inputs_pw).outputs.remote_folder
+
+    builder = HpBaseWorkChain.get_builder_from_protocol(code, parent_scf_folder=parent_scf_folder)
+    assert builder.hp.parent_scf == parent_scf_folder
+
+
+def test_parent_hp_folders(fixture_code, generate_calc_job_node):
+    """Test ``HpBaseWorkChain.get_builder_from_protocol`` with ``parent_hp_folders`` keyword."""
+    code = fixture_code('quantumespresso.hp')
+    parent_hp_folders = {'site_01': generate_calc_job_node('quantumespresso.hp').outputs.retrieved}
+
+    builder = HpBaseWorkChain.get_builder_from_protocol(code, parent_hp_folders=parent_hp_folders)
+    assert 'parent_hp' in builder.hp
+    assert 'site_01' in builder.hp.parent_hp
+    assert builder.hp.parent_hp['site_01'] == parent_hp_folders['site_01']
+
+
+def test_parameter_overrides(fixture_code):
+    """Test specifying parameter ``overrides`` for the ``get_builder_from_protocol()`` method."""
+    code = fixture_code('quantumespresso.hp')
+
+    overrides = {'hp': {'parameters': {'INPUTHP': {'conv_thr_chi': 1}}}}
+    builder = HpBaseWorkChain.get_builder_from_protocol(code, overrides=overrides)
+    assert builder.hp.parameters['INPUTHP']['conv_thr_chi'] == 1  # pylint: disable=no-member
+
+
+def test_settings_overrides(fixture_code):
+    """Test specifying settings ``overrides`` for the ``get_builder_from_protocol()`` method."""
+    code = fixture_code('quantumespresso.hp')
+
+    overrides = {'hp': {'settings': {'cmdline': ['--kickass-mode']}}}
+    builder = HpBaseWorkChain.get_builder_from_protocol(code, overrides=overrides)
+    assert builder.hp.settings['cmdline'] == ['--kickass-mode']  # pylint: disable=no-member
+    assert builder.hp.settings['parent_folder_symlink']
+
+
+def test_metadata_overrides(fixture_code):
+    """Test specifying metadata ``overrides`` for the ``get_builder_from_protocol()`` method."""
+    code = fixture_code('quantumespresso.hp')
+
+    overrides = {'hp': {'metadata': {'options': {'resources': {'num_machines': 1e90}, 'max_wallclock_seconds': 1}}}}
+    builder = HpBaseWorkChain.get_builder_from_protocol(
+        code,
+        overrides=overrides,
+    )
+    metadata = builder.hp.metadata  # pylint: disable=no-member
+
+    assert metadata['options']['resources']['num_machines'] == 1e90
+    assert metadata['options']['max_wallclock_seconds'] == 1
+
+
+def test_options(fixture_code):
+    """Test specifying ``options`` for the ``get_builder_from_protocol()`` method."""
+    code = fixture_code('quantumespresso.hp')
+
+    queue_name = 'super-fast'
+    withmpi = False  # The protocol default is ``True``
+
+    options = {'queue_name': queue_name, 'withmpi': withmpi}
+    builder = HpBaseWorkChain.get_builder_from_protocol(code, options=options)
+    metadata = builder.hp.metadata  # pylint: disable=no-member
+
+    assert metadata['options']['queue_name'] == queue_name
+    assert metadata['options']['withmpi'] == withmpi
