89d8b8d4f53fdfe14cc450ac32b5f8d9a6a8eae8
bastonero <bastonero.lorenzo@gmail.com>
2023-05-01 14:52:48 +0000

    Utils: add useful general functions
    Add the following methods:
    * `set_tot_magnetization`: useful for setting the
       total magnetization to an integer value, otherwise
       QE will complain
    * `is_perturb_only_atom`: used to detect the "perturb_only_atom"
       key in the hp.parameters (used in different validations)

---
+++ tests/utils/test_general.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""Tests for the :mod:`aiida_quantumespresso_hp.utils.general` module."""
+
+
+def test_set_tot_magnetization():
+    """Test the `set_tot_magnetization` function."""
+    from aiida_quantumespresso_hp.utils.general import set_tot_magnetization
+
+    parameters = {'SYSTEM': {}}
+
+    assert set_tot_magnetization(parameters, 0.1)
+    assert parameters['SYSTEM']['tot_magnetization'] == 0
+
+    parameters = {'SYSTEM': {}}
+
+    assert not set_tot_magnetization(parameters, 0.5)
+
+
+def test_is_perturb_only_atom():
+    """Test the `is_perturb_only_atom` function."""
+    from aiida_quantumespresso_hp.utils.general import is_perturb_only_atom
+
+    parameters = {}
+    assert is_perturb_only_atom(parameters) is None
+
+    parameters = {'perturb_only_atom(1)': True}
+    assert is_perturb_only_atom(parameters) == 1
+
+    parameters = {'perturb_only_atom(1)': False}
+    assert is_perturb_only_atom(parameters) is None
