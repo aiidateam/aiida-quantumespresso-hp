1e7ede1eb31b492d39a7f495f8e623eaf462b8ed
bastonero <bastonero.lorenzo@gmail.com>
2023-05-02 09:32:19 +0000

    `HpWorkChain`: update and improvements
    The following changes are applied:
    * Introduction of `clean_workdir` input
    * Introduction of `parallelize_qpoints` input
    * The `qpoints_distance` input, along method in the outline
       is added in order to automatically assign the q point grid
       given a distance between points in reciprocal space
    * The `get_builder_from_protocol` is added to fill automatically
       a builder ready to submit

--- src/aiida_quantumespresso_hp/workflows/hp/main.py
+++ src/aiida_quantumespresso_hp/workflows/hp/main.py
@@ -1,19 +1,32 @@
 # -*- coding: utf-8 -*-
 """Work chain to run a Quantum ESPRESSO hp.x calculation."""
 from aiida import orm
+from aiida.common import AttributeDict
 from aiida.engine import ToContext, WorkChain, if_
 from aiida.plugins import WorkflowFactory
+from aiida_quantumespresso.workflows.protocols.utils import ProtocolMixin
 
 HpBaseWorkChain = WorkflowFactory('quantumespresso.hp.base')
 HpParallelizeAtomsWorkChain = WorkflowFactory('quantumespresso.hp.parallelize_atoms')
 
 
-class HpWorkChain(WorkChain):
+def validate_inputs(inputs, _):
+    """Validate the top level namespace."""
+    if inputs['parallelize_qpoints']:
+        if not inputs['parallelize_atoms']:
+            return 'To use `parallelize_qpoints`, also `parallelize_atoms` must be `True`'
+
+
+class HpWorkChain(WorkChain, ProtocolMixin):
     """Work chain to run a Quantum ESPRESSO hp.x calculation.
 
     If the `parallelize_atoms` input is set to `True`, the calculation will be parallelized over the Hubbard atoms by
-    running the `HpParallelizeAtomsWorkChain`. Otherwise a single `HpBaseWorkChain` will be launched that will compute
-    every Hubbard atom in serial.
+    running the `HpParallelizeAtomsWorkChain`. When parallelizing over atoms, if the `parallelize_qpoints` is `True`,
+    each `HpParallelizeAtomsWorkChain` will be parallelized over its perturbations (q points) running the
+    `HpParallelizeQpointsWorkChain`. Otherwise a single `HpBaseWorkChain` will be launched that will compute
+    every Hubbard atom, and every q point in serial.
+
+    .. important:: q point parallelization is only possible when parallelization over atoms is performed.
     """
 
     @classmethod
@@ -21,9 +34,22 @@ class HpWorkChain(WorkChain):
         """Define the process specification."""
         # yapf: disable
         super().define(spec)
-        spec.expose_inputs(HpBaseWorkChain, exclude=('only_initialization',))
+        spec.expose_inputs(HpBaseWorkChain, exclude=('clean_workdir', 'only_initialization', 'hp.qpoints'))
+        spec.input('clean_workdir', valid_type=orm.Bool, default=lambda: orm.Bool(False),
+            help='If `True`, work directories of all called calculation will be cleaned at the end of execution.')
+        spec.input('qpoints', valid_type=orm.KpointsData, required=False,
+            help='An explicit q-points list or mesh. Either this or `qpoints_distance` has to be provided.')
+        spec.input('qpoints_distance', valid_type=orm.Float, required=False,
+            help='The minimum desired distance in 1/Å between q-points in reciprocal space. The explicit q-points will '
+            'be generated automatically by a calculation function based on the input structure.')
+        spec.input('qpoints_force_parity', valid_type=orm.Bool, required=False,
+            help='Optional input when constructing the q-points based on a desired `qpoints_distance`. Setting this to '
+            '`True` will force the q-point mesh to have an even number of points along each lattice vector except '
+            'for any non-periodic directions.')
         spec.input('parallelize_atoms', valid_type=orm.Bool, default=lambda: orm.Bool(False))
+        spec.input('parallelize_qpoints', valid_type=orm.Bool, default=lambda: orm.Bool(False))
         spec.outline(
+            cls.validate_qpoints,
             if_(cls.should_parallelize_atoms)(
                 cls.run_parallel_workchain,
             ).else_(
@@ -32,8 +58,92 @@ class HpWorkChain(WorkChain):
             cls.inspect_workchain,
             cls.results,
         )
+        spec.inputs.validator = validate_inputs
         spec.expose_outputs(HpBaseWorkChain)
+        spec.exit_code(200, 'ERROR_INVALID_INPUT_QPOINTS',
+            message=('Neither the `qpoints` nor the `qpoints_distance`, '
+                'or the `hp.hubbard_structure` input were specified.'))
         spec.exit_code(300, 'ERROR_CHILD_WORKCHAIN_FAILED', message='A child work chain failed.')
+
+    @classmethod
+    def get_protocol_filepath(cls):
+        """Return ``pathlib.Path`` to the ``.yaml`` file that defines the protocols."""
+        from importlib_resources import files
+
+        from ..protocols import hp as hp_protocols
+        return files(hp_protocols) / 'main.yaml'
+
+    @classmethod
+    def get_builder_from_protocol(cls, code, protocol=None, parent_scf_folder=None, overrides=None, options=None, **_):
+        """Return a builder prepopulated with inputs selected according to the chosen protocol.
+
+        :param code: the ``Code`` instance configured for the ``quantumespresso.hp`` plugin.
+        :param protocol: protocol to use, if not specified, the default will be used.
+        :param parent_scf_folder: the parent ``RemoteData`` of the respective SCF calcualtion.
+        :param overrides: optional dictionary of inputs to override the defaults of the protocol.
+        :param options: A dictionary of options that will be recursively set for the ``metadata.options`` input of all
+            the ``CalcJobs`` that are nested in this work chain.
+        :return: a process builder instance with all inputs defined ready for launch.
+        """
+        inputs = cls.get_protocol_inputs(protocol, overrides)
+
+        data = HpBaseWorkChain.get_builder_from_protocol(  # pylint: disable=protected-access
+            code, protocol=protocol, parent_scf_folder=parent_scf_folder, overrides=inputs, options=options, **_
+        )._data
+
+        data.pop('only_initialization', None)
+        data['hp'].pop('qpoints', None)
+
+        if 'qpoints' in inputs:
+            qpoints = orm.KpointsData()
+            qpoints.set_kpoints_mesh(inputs['qpoints'])
+            data['qpoints'] = qpoints
+        if 'qpoints_distance' in inputs:
+            data['qpoints_distance'] = orm.Float(inputs['qpoints_distance'])
+        if 'qpoints_force_parity' in inputs:
+            data['qpoints_force_parity'] = orm.Bool(inputs['qpoints_force_parity'])
+        if 'parallelize_atoms' in inputs:
+            data['parallelize_atoms'] = orm.Bool(inputs['parallelize_atoms'])
+        if 'parallelize_qpoints' in inputs:
+            data['parallelize_qpoints'] = orm.Bool(inputs['parallelize_qpoints'])
+
+        builder = cls.get_builder()
+        builder._data = data  # pylint: disable=protected-access
+        builder.clean_workdir = orm.Bool(inputs['clean_workdir'])
+
+        return builder
+
+    def validate_qpoints(self):
+        """Validate the inputs related to q-points.
+
+        Either an explicit `KpointsData` with given mesh/path, or a desired q-points distance should be specified. In
+        the case of the latter, the `KpointsData` will be constructed for the input `StructureData` using the
+        `create_kpoints_from_distance` calculation function.
+        """
+        from aiida_quantumespresso.calculations.functions.create_kpoints_from_distance import (
+            create_kpoints_from_distance,
+        )
+
+        if all(key not in self.inputs for key in ['qpoints', 'qpoints_distance']):
+            return self.exit_codes.ERROR_INVALID_INPUT_QPOINTS
+
+        try:
+            qpoints = self.inputs.qpoints
+        except AttributeError:
+            if 'hubbard_structure' in self.inputs.hp:
+                inputs = {
+                    'structure': self.inputs.hp.hubbard_structure,
+                    'distance': self.inputs.qpoints_distance,
+                    'force_parity': self.inputs.get('qpoints_force_parity', orm.Bool(False)),
+                    'metadata': {
+                        'call_link_label': 'create_qpoints_from_distance'
+                    }
+                }
+                qpoints = create_kpoints_from_distance(**inputs)  # pylint: disable=unexpected-keyword-arg
+            else:
+                return self.exit_codes.ERROR_INVALID_INPUT_QPOINTS
+
+        self.ctx.qpoints = qpoints
 
     def should_parallelize_atoms(self):
         """Return whether the calculation should be parallelized over atoms."""
@@ -41,13 +151,19 @@ class HpWorkChain(WorkChain):
 
     def run_base_workchain(self):
         """Run the `HpBaseWorkChain`."""
-        running = self.submit(HpBaseWorkChain, **self.exposed_inputs(HpBaseWorkChain))
+        inputs = AttributeDict(self.exposed_inputs(HpBaseWorkChain))
+        inputs.hp.qpoints = self.ctx.qpoints
+        running = self.submit(HpBaseWorkChain, **inputs)
         self.report(f'running in serial, launching HpBaseWorkChain<{running.pk}>')
         return ToContext(workchain=running)
 
     def run_parallel_workchain(self):
         """Run the `HpParallelizeAtomsWorkChain`."""
-        running = self.submit(HpParallelizeAtomsWorkChain, **self.exposed_inputs(HpBaseWorkChain))
+        inputs = AttributeDict(self.exposed_inputs(HpBaseWorkChain))
+        inputs.clean_workdir = self.inputs.clean_workdir
+        inputs.parallelize_qpoints = self.inputs.parallelize_qpoints
+        inputs.hp.qpoints = self.ctx.qpoints
+        running = self.submit(HpParallelizeAtomsWorkChain, **inputs)
         self.report(f'running in parallel, launching HpParallelizeAtomsWorkChain<{running.pk}>')
         return ToContext(workchain=running)
 
@@ -60,3 +176,24 @@ class HpWorkChain(WorkChain):
     def results(self):
         """Retrieve the results from the completed sub workchain."""
         self.out_many(self.exposed_outputs(self.ctx.workchain, HpBaseWorkChain))
+
+    def on_terminated(self):
+        """Clean the working directories of all child calculations if `clean_workdir=True` in the inputs."""
+        super().on_terminated()
+
+        if self.inputs.clean_workdir.value is False:
+            self.report('remote folders will not be cleaned')
+            return
+
+        cleaned_calcs = []
+
+        for called_descendant in self.node.called_descendants:
+            if isinstance(called_descendant, orm.CalcJobNode):
+                try:
+                    called_descendant.outputs.remote_folder._clean()  # pylint: disable=protected-access
+                    cleaned_calcs.append(called_descendant.pk)
+                except (IOError, OSError, KeyError):
+                    pass
+
+        if cleaned_calcs:
+            self.report(f"cleaned remote folders of calculations: {' '.join(map(str, cleaned_calcs))}")
---
+++ tests/workflows/hp/test_main.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=no-member,redefined-outer-name
+"""Tests for the `HpWorkChain` class."""
+from plumpy import ProcessState
+import pytest
+
+from aiida_quantumespresso_hp.workflows.hp.main import HpWorkChain
+
+
+@pytest.fixture
+def generate_workchain_main(generate_workchain, generate_inputs_hp, generate_hubbard_structure):
+    """Generate an instance of a `HpWorkChain`."""
+
+    def _generate_workchain_main(inputs=None, atoms=True, qpoints=True, qdistance=True):
+        from aiida.orm import Bool, Float
+
+        entry_point = 'quantumespresso.hp.main'
+
+        inputs = generate_inputs_hp(inputs=inputs)
+        inputs['hubbard_structure'] = generate_hubbard_structure()
+
+        workchain_inputs = {
+            'hp': inputs,
+            'parallelize_atoms': Bool(atoms),
+            'parallelize_qpoints': Bool(qpoints),
+        }
+
+        if qdistance:
+            workchain_inputs['qpoints_distance'] = Float(0.15)
+        else:
+            workchain_inputs['qpoints'] = workchain_inputs['hp'].pop('qpoints')
+
+        process = generate_workchain(entry_point, workchain_inputs)
+
+        return process
+
+    return _generate_workchain_main
+
+
+@pytest.fixture
+def generate_hp_workchain_node(generate_calc_job_node):
+    """Generate an instance of `WorkflowNode`."""
+
+    def _generate_hp_workchain_node(exit_status=0, use_retrieved=False):
+        from aiida.common import LinkType
+        from aiida.orm import Dict, WorkflowNode
+
+        node = WorkflowNode().store()
+        node.set_process_state(ProcessState.FINISHED)
+        node.set_exit_status(exit_status)
+
+        parameters = Dict({'number_of_qpoints': 2}).store()
+        parameters.base.links.add_incoming(node, link_type=LinkType.RETURN, link_label='parameters')
+
+        if use_retrieved:
+            retrieved = generate_calc_job_node(
+                'quantumespresso.hp'
+            ).outputs.retrieved  # otherwise the HpCalculation will complain
+            retrieved.base.links.add_incoming(node, link_type=LinkType.RETURN, link_label='retrieved')
+
+        return node
+
+    return _generate_hp_workchain_node
+
+
+def test_validate_inputs_invalid(generate_workchain_main):
+    """Test `HpWorkChain.validate_inputs` with invalid inputs."""
+    match = r'To use `parallelize_qpoints`, also `parallelize_atoms` must be `True`'
+    with pytest.raises(ValueError, match=match):
+        generate_workchain_main(atoms=False, qpoints=True)
+
+
+@pytest.mark.usefixtures('aiida_profile')
+def test_validate_qpoints(generate_workchain_main):
+    """Test `HpWorkChain.validate_qpoints`."""
+    process = generate_workchain_main()
+    process.validate_qpoints()
+    assert 'qpoints' in process.ctx
+
+    process = generate_workchain_main(qdistance=True)
+    assert process.validate_qpoints() is None
+    assert 'qpoints' in process.ctx
+
+
+@pytest.mark.usefixtures('aiida_profile')
+def test_should_parallelize_atoms(generate_workchain_main):
+    """Test `HpWorkChain.should_parallelize_atoms`."""
+    process = generate_workchain_main()
+    process.validate_qpoints()
+    assert process.should_parallelize_atoms()
+
+
+@pytest.mark.usefixtures('aiida_profile')
+def test_run_base_workchain(generate_workchain_main):
+    """Test `HpWorkChain.run_base_workchain`."""
+    process = generate_workchain_main()
+    process.validate_qpoints()
+    result = process.run_base_workchain()
+    assert result is not None
+
+
+@pytest.mark.usefixtures('aiida_profile')
+def test_run_parallel_workchain(generate_workchain_main):
+    """Test `HpWorkChain.run_parallel_workchain`."""
+    process = generate_workchain_main()
+    process.validate_qpoints()
+    result = process.run_parallel_workchain()
+    assert result is not None
+
+
+@pytest.mark.usefixtures('aiida_profile')
+def test_inspect_workchain(generate_workchain_main, generate_hp_workchain_node):
+    """Test `HpWorkChain.inspect_workchain`."""
+    process = generate_workchain_main()
+    process.validate_qpoints()
+    process.ctx.workchain = generate_hp_workchain_node(exit_status=300)
+
+    result = process.inspect_workchain()
+    assert result == HpWorkChain.exit_codes.ERROR_CHILD_WORKCHAIN_FAILED
