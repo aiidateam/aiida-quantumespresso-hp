e196fb3cef6113e57c1a23230f1f7b85f19e11f3
bastonero <bastonero.lorenzo@gmail.com>
2023-11-14 16:20:49 +0000

    Address some python warning on typing

--- src/aiida_quantumespresso_hp/calculations/functions/structure_relabel_kinds.py
+++ src/aiida_quantumespresso_hp/calculations/functions/structure_relabel_kinds.py
@@ -13,7 +13,7 @@ from aiida_quantumespresso.data.hubbard_structure import HubbardStructureData
 def structure_relabel_kinds(
     hubbard_structure: HubbardStructureData,
     hubbard: Dict,
-    magnetization: dict | None = None,
+    magnetization: Dict | None = None,
 ) -> Dict:
     """Create a clone of the given structure but with new kinds, based on the new hubbard sites.
 
